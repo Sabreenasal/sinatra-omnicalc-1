ddf761c220bf46435a6b325cc0678436989e5a70
Sabreena Salomon <salomonbree@gmail.com>
2025-01-06 20:10:51 +0000

    added new erb routes

--- app.rb
+++ app.rb
@@ -2,8 +2,40 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:new_square_calc)
+end
+
+get("/square/new") do
+erb(:new_square_calc)
+end
+
+get "/square/results" do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 2
+ erb(:square_results)
+end
+
+get "/square_root/new" do
+ erb(:new_square_root)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = Math.sqrt(@the_num)
+  erb(:root_results)
+end
+
+get("/random/new") do
+ erb(:random)
+end
+
+get("/random/results") do
+  @the_min = params.fetch("min_input").to_f
+
+  @the_max = params.fetch("max_input").to_f
+
+  @the_rand_num = 
+ erb(:random_results)
 end
